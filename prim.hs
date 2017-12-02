74c607e24af2e7fb641045b8ca910b888c71f18c
rachellaserzhao <ruiqirachelzhao@gmail.com>
2017-12-01 19:44:34 -0800

    prim

--- prim.hs
+++ prim.hs
@@ -32,42 +32,37 @@ v1 (Edge (v1,_) _) = v1
 v2 :: Edge -> Vertex
 v2 (Edge (_,v2) _) = v2
 
--- graph contains a list of vertex and a list of edges--
 data Graph = Graph [Vertex] [Edge] deriving (Show)
 
 
------ prim's algorithm minimum spanning tree-----
+----- Prim's theorem minimum spanning tree-----
 
 ----accessors----
 
--- determine if the a given vertex is a member of the list of vertices
+-- determine if the a given vertex is a member of the list of Labels of vertices
+hasVertex :: Vertex -> [String] -> Bool
 hasVertex _ []  = False
 hasVertex v (x:y) 
      | label v == x = True
      | otherwise = hasVertex v y
 
--- get a list of all vertices from a graph according to labels
-labelsToVertices (Graph [] _) _ = []
-labelsToVertices (Graph (x:y) _) [] = x:y
-labelsToVertices (Graph (x:y) _) labels = filter (\ a -> label a `elem` labels) (x:y)
--- If either `s` (seen) or `vn` (vertex neighbors) are empty, return an empty list.
+-- remove vertices whose label is in list of label l from list of vertices q
+removeLabeled :: [Vertex] -> [String] -> [Vertex]
 removeLabeled _ [] = []
 removeLabeled [] _ = []
--- Filter out all vertexes in `vn` that are also in `s`.
 removeLabeled q l = filter (\ x -> not $ hasVertex x l) q
 
--- Go though each vertex and swap the current distance and predecessor labels with the new parameters.
-updatePred (Vertex l a d p) vLabel = (Vertex l a d vLabel)
+-- update each labeled vertex by adding their predecessor, they are on either end of the minimum weighed edge
+updatePred :: Vertex -> String -> Vertex
+updatePred (Vertex l a d p) pred = (Vertex l a d pred)
 
--- initialize graph to label evey vertex distance zero
-setDist lst = [ (Vertex s lst  0 s2) | (Vertex s lst int s2)<- lst]
-
-merge vLabel egIn = [(Edge(a,b) i) | (Edge (a,b) i) <- egIn, 
+-- get a list of edges from a given vertex -- 
+getEdgeList :: String -> [Edge] -> [Edge]
+getEdgeList vLabel egIn = [(Edge(a,b) i) | (Edge (a,b) i) <- egIn, 
           (label a == vLabel || label b == vLabel)]
 
-{-
-(notInPairList (a,b) (getPairs seen)))]
--}
+-- can potentially be used for implementing a set to log all minimum edges--
+-- a set is not implemented here --
 notInPairList (a,b) [] = True
 
 notInPairList (a,b) ((x,y):rest) 
@@ -76,69 +71,70 @@ notInPairList (a,b) ((x,y):rest)
 
 getPairs eList = [(x,y) | (Edge (x,y) i) <- eList]
 
-
-findMin (Graph (x:y) (xe:ye)) labeled l = 
-     edgeMin [(Edge (a,b) i) | (Edge (a,b) i) <- (xe:ye), 
-               ((label a ==l && (label b `notElem` vertexToLabel labeled)) || (label b ==l && (label a `notElem` vertexToLabel labeled)))] 
-               xe
-
-vertexToLabel []= []
-vertexToLabel lst = [label x | x <- lst]
-
-
+-- call to find the minimum weighed edge from a list of edges that has been seen but not used
+getMinEdge :: [Edge] -> [String] -> Edge
 getMinEdge [] _ = error "list cannot be empty"
 getMinEdge (x:y) [] = edgeMin (x:y) x
 getMinEdge le lv = edgeMin (removeMin le lv) (head (removeMin le lv))
 
+-- filter out edges that connects two already labeled vertices-- 
+removeMin :: [Edge] -> [String] -> [Edge]
 removeMin le lv = [(Edge(a,b) i) | (Edge (a,b) i) <- le, 
           ((label a `notElem` lv) || (label b `notElem` lv))]
 
-edgeMin [] min = min
+-- find the minimum weighted edge -- 
+edgeMin :: [Edge] -> Edge -> Edge
 
+edgeMin [] min = min
 edgeMin (a:b) min 
      | (weight a <= weight min) = edgeMin b a
      | otherwise = edgeMin b min
 
+-- access vertex from edge 
+toNextVertex :: Edge -> [String] -> Vertex
+
 toNextVertex (Edge (a,b) _) lst
      | ((label a) `elem` lst) = b
      | otherwise = a
+
+toPrevVertex :: Edge -> [String] -> Vertex
 
 toPrevVertex (Edge (a,b) _) lst
      | ((label a) `elem` lst) = a
      | otherwise = b
 
 --- Prim's algorithm ---
----prim :: Graph -> Graph -> [Vertex] -> [Vertex] -> Vertex -> Graph
+-- argument: input graph, output graph accumulator, a queue initalized with all vertices in a graph,
+-- a starting vertex, an edge list accumulator keeping the incident edges of every vertex seen so far
+-- a list of vertices that has been reached by the minimum spannig tree
+prim :: Graph -> Graph -> [Vertex] -> Vertex -> [Edge] -> [String] -> Graph
 --base case :empty graph
+prim (Graph [] lst) _ _ _ _ _ = Graph [] lst
 
---prim (Graph [] lst) _ _ _ _ _ = Graph [] lst
-
---when reached empty queue
+--when reaching queue containing only one element, meaning all vertices has been travered, return outGraph accumulator
 prim _ outGraph [a] vertex seenEdge seenVertex = outGraph
 
 prim (Graph verIn egIn) (Graph ver eg) q vi se sv = prim inGraph outGraph queue v seenEdge seenVertex
     where inGraph = Graph verIn egIn
           -- Get the current vertex label.
           vLabel = label vi
-          -- add seen vertex label into list of seenV
+          -- add this vertex label into list of seenV
           seenV = vLabel:sv
-          -- add seen edge into list of seenE which accumulated as we are traversing
-          seenE = (merge vLabel egIn)++se
- 
-          -- find the minimun weighted edges from list seen so far
-          minEdge = getMinEdge seenE seenV           
-          -- minEdge = findMin inGraph labeled vLabel
-          
+          -- add all incident edges of current vertex into list of seenE
+          seenE = (getEdgeList vLabel egIn)++se 
+          -- find the minimun weighted edges from list of unused edges seen so far
+          minEdge = getMinEdge seenE seenV                     
           -- get the next vertex we haven't seen 
           nextVertex = toNextVertex minEdge seenV
-          
+          -- get its predecessor
           prevVertex = toPrevVertex minEdge seenV
-          
+          -- update this vertex by addeing its predecessor
           addToGraph = updatePred nextVertex (label prevVertex)
 
--- update accumulators for the next loop
+          -- update accumulators for the next loop
           outGraph = Graph (addToGraph:ver) (minEdge:eg) 
           v = nextVertex
+          --remove vertices that has been labeled
           queue = removeLabeled q seenV
           seenEdge = seenE
           seenVertex = seenV
@@ -176,6 +172,8 @@ stober = Edge (sto, ber) 504
 warhel = Edge (war, hel) 570
 helsto = Edge (hel, sto) 246
 
+europe_edge = Graph [bar,mil,mad,pra,par,lon,ber,war,hel,sto] [barmil,barpar,barlon,barmad,milpar,parlon,madlon,milpra,milber,parber,berlon,praber,stolon,warber,helber,stober,warhel,helsto] 
+
 osa = Vertex "Osaka" ["Kyoto"] 1000 ""
 kyo = Vertex "Kyoto" ["Osaka", "Nagoya", "Tokyo"] 1000 ""
 nag = Vertex "Nagoya" ["Kyoto", "Tokyo"] 1000 ""
@@ -186,17 +184,20 @@ kyonag = Edge (kyo,nag) 65
 kyotok = Edge (kyo,tok) 226
 toknag = Edge (tok,nag) 160
 
-
-europe_edge = Graph [bar,mil,mad,pra,par,lon,ber,war,hel,sto] [barmil,barpar,barlon,barmad,milpar,parlon,madlon,milpra,milber,parber,berlon,praber,stolon,warber,helber,stober,warhel,helsto] 
-
-
+getEV (Graph _ le) = [[(label a), (label b), show(i)] | (Edge (a,b) i) <- le] 
 
 mst = 
-	do
-	    	
-          let result = prim (Graph [ber,war,hel,sto] [warber,helber,stober,warhel,helsto]) (Graph [ber] []) [ber,war,hel,sto] ber [] [] 
-		print result
-		return()
+
+    do
+         putStrLn "Please enter the map to load:"
+         putStrLn "(1.Europe 2.Japan)"
+         country <- getLine	    
+         let result = (choose country)
+         let edges = getEV result
+         print edges
+         return()
 
 
-
+choose a 
+     | a == "1" = prim (Graph [ber,war,hel,sto] [warber,helber,stober,warhel,helsto]) (Graph [ber] []) [ber,war,hel,sto] ber [] []
+     | otherwise = prim (Graph [osa,kyo,nag,tok] [osakyo,kyonag,kyotok,toknag]) (Graph [osa] []) [osa,kyo,nag,tok] osa [] []
